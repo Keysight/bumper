0acb28b3fbcb5a7a34715009bd49cb5130913ef5
Arjen Rouvoet <rouvoet@riscure.com>
2022-09-20 17:41:17 +0200

    Add test expectations for declaration test

--- riscure-truecode-frontend/src/test/kotlin/com/riscure/langs/C/parser/ClangParserTest.kt
+++ riscure-truecode-frontend/src/test/kotlin/com/riscure/langs/C/parser/ClangParserTest.kt
@@ -6,6 +6,7 @@ import com.riscure.tc.codeanalysis.clang.compiler2.loader.CompileCommandMapping
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.util.*
 
 class ClangParserTest() {
 
@@ -40,7 +41,25 @@ class ClangParserTest() {
     fun parseExample02() {
         val c02 = File(ClangParserTest::class.java.getResource("/examples/ex02.c")!!.file)
         parsed(c02) { tu ->
-            println(tu)
+            assertTrue(tu.decls[0] is GlobalDecl.Fun)
+
+            val fn = tu.decls[0] as GlobalDecl.Fun
+            assertTrue(fn.body is Stmt.Block)
+
+            val b = fn.body as Stmt.Block
+            assertEquals(2, b.stmts.size)
+
+            val s1 = b.stmts[0]
+            val s2 = b.stmts[1]
+            assertTrue(s1 is Stmt.Decl)
+            assertTrue(s2 is Stmt.Decl)
+
+            val d1 = s1 as Stmt.Decl
+            val d2 = s2 as Stmt.Decl
+            assertEquals("i", d1.name)
+            assertEquals(Type.Int(IKind.IInt), d1.type)
+            assertEquals("array", d2.name)
+            assertEquals(Type.Array(Type.Int(IKind.IInt), Optional.of(8)), d2.type)
         }
     }
 }
---
+++ riscure-truecode-frontend/src/test/resources/examples/ex02.c
@@ -0,0 +1,4 @@
+void test() {
+   int i;
+   int array[8];
+}
