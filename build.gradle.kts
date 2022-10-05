8e83768b955e2c73662e9a0a7fc6ddf66cc5014b
Arjen Rouvoet <rouvoet@riscure.com>
2022-10-05 15:24:20 +0200

    Fix dependencies and repository config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,22 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 description = "Riscure True Code C Frontend"
 
 plugins {
-    id("com.riscure.java-conventions")
+    `maven-publish`
     kotlin("jvm") version "1.7.10"
+}
+
+group   = "com.riscure"
+version = "0.1.0-SNAPSHOT"
+
+val releases  = uri("http://nexus3.riscure.com:8081/repository/riscure")
+val snapshots = uri("http://nexus3.riscure.com:8081/repository/riscure-snapshots")
+
+repositories {
+    maven { url = releases ; isAllowInsecureProtocol = true }
+    maven { url = snapshots; isAllowInsecureProtocol = true }
+
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
@@ -16,9 +30,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.0.1")
     implementation("com.github.pgreze:kotlin-process:1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-
-    implementation(project(":tc-codeanalysis"))
-    implementation(project(":dobby"))
+    implementation("com.riscure:dobby:0.1.0-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation(kotlin("test"))
@@ -43,5 +55,40 @@ tasks.compileTestKotlin {
     kotlinOptions {
         jvmTarget = "1.8"
         freeCompilerArgs += "-Xcontext-receivers"
+    }
+}
+
+// Publishing
+
+fun env(key: String): String? = System.getenv(key)
+
+val nexusUsername = env("NEXUS_USERNAME")
+val nexusPassword = env("NEXUS_PASSWORD")
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId    = "com.riscure"
+            artifactId = rootProject.name
+            version    = version
+
+            from(components["java"])
+
+            pom {
+                name.set(rootProject.name)
+                description.set("A C frontend")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshots else releases)
+            isAllowInsecureProtocol = true
+            credentials {
+                username = nexusUsername
+                password = nexusPassword
+            }
+        }
     }
 }
