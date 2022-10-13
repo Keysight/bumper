49ea49f28246b45029819a69a421880961fa6f7b
Arjen Rouvoet <rouvoet@riscure.com>
2022-10-13 13:18:05 +0200

    Make buildable with maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("org.bytedeco:llvm-platform:11.0.0-1.5.5-SNAPSHOT")
 
     implementation("org.slf4j:slf4j-api:1.7.25")
-    implementation("io.arrow-kt:arrow-core:1.0.1")
+    implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.github.pgreze:kotlin-process:1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation(project(":dobby"))
