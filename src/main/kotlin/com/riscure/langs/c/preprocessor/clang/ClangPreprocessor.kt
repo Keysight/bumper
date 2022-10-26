8b848453180a3a6a39183fdce61ab76a02081d0e
Arjen Rouvoet <rouvoet@riscure.com>
2022-10-26 13:16:12 +0200

    Fix for dobby changes

--- src/main/kotlin/com/riscure/langs/c/preprocessor/clang/ClangPreprocessor.kt
+++ src/main/kotlin/com/riscure/langs/c/preprocessor/clang/ClangPreprocessor.kt
@@ -25,15 +25,14 @@ class ClangPreprocessor(private val clang: Path) : Preprocessor {
             Preprocessor.Error(main, "Preprocessor expects input with '.c' file extension, got '$main'").left()
         }
 
-        val cmd: Command = with(Spec.clang11) {
+        val cmd: Command =
              Command(opts, listOf(main.absolutePath))
                 // replacing any existing output options
-                .replace(Arg(Spec.clang11["o"], listOf(out.absolutePath)))
+                .replace(Spec.clang11, Arg(Spec.clang11["o"], listOf(out.absolutePath)))
                 // We add -O0, which really should be implied by -E, but it isn't.
                 // It seems that in clang-16 it is.
                 .plus(Arg(Spec.clang11["O0"]))
                 .plus(Arg(Spec.clang11["E"]))
-        }
 
         log.info("Exec: $clang ${cmd.toArguments().joinToString(separator = " ")}")
 
