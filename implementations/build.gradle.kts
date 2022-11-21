6affdd87e9d765d3b91af51c64bbd0add75f6c90
Arjen Rouvoet <rouvoet@riscure.com>
2022-11-21 14:15:17 +0100

    Add libclang implementation gradle build

---
+++ implementations/build.gradle.kts
@@ -0,0 +1,60 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+description = "Riscure True Code C Frontend -- Libclang Implementation"
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    `maven-publish`
+}
+
+group   = "com.riscure"
+version = "0.1.0-SNAPSHOT"
+
+val releases  = uri("http://nexus3.riscure.com:8081/repository/riscure")
+val snapshots = uri("http://nexus3.riscure.com:8081/repository/riscure-snapshots")
+
+repositories {
+    maven { url = releases ; isAllowInsecureProtocol = true }
+    maven { url = snapshots; isAllowInsecureProtocol = true }
+
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("org.bytedeco:llvm-platform:11.0.0-1.5.5-SNAPSHOT")
+
+    implementation("io.arrow-kt:arrow-core:1.1.2")
+    implementation("com.github.pgreze:kotlin-process:1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+
+    implementation("com.riscure:dobby:0.1.0-SNAPSHOT")
+    implementation(project(":bumper-core"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation(project(":bumper-test"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+  testLogging {
+    events(PASSED, FAILED, STANDARD_OUT, STANDARD_ERROR, SKIPPED)
+    exceptionFormat = FULL
+  }
+}
+
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers" + "-Xskip-prerelease-check"
+    }
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers" + "-Xskip-prerelease-check"
+    }
+}
