bf5e704e409202fbd66cfc468b23aec685a273fa
Diego Rivera <rivera@riscure.com>
2022-12-27 11:48:45 +0100

    TC-2859 Added missing dependency

--- implementations/libclang/build.gradle.kts
+++ implementations/libclang/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.github.pgreze:kotlin-process:1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("info.picocli:picocli:4.6.3")
 
     implementation("com.riscure:riscure-dobby:0.1.0-SNAPSHOT")
     implementation(project(":bumper-core"))
