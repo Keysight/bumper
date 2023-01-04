a26cf0ae669f2237549cf4d5db25ce390f01bee3
Arjen Rouvoet <rouvoet@riscure.com>
2023-01-04 15:14:26 +0100

    Configure 0.1.0 release

--- implementations/libclang/build.gradle.kts
+++ implementations/libclang/build.gradle.kts
@@ -13,40 +13,6 @@ application {
     mainClass.set("com.riscure.bumper.BumperCmdKt")
 }
 
-fun systemProperty(key: String): String? = System.getProperty(key)
-
-val bambooLocal = systemProperty("bambooMavenLocalRepo") ?: "../../../.repo/"
-
-group   = "com.riscure"
-version = "0.1.0-SNAPSHOT"
-
-val releases  = uri("http://nexus3.riscure.com:8081/repository/riscure")
-val snapshots = uri("http://nexus3.riscure.com:8081/repository/riscure-snapshots")
-
-repositories {
-    maven { url = releases ; isAllowInsecureProtocol = true }
-    maven { url = snapshots; isAllowInsecureProtocol = true }
-
-    maven {
-        url = uri(bambooLocal)
-        name = "localBamboo"
-    }
-
-    // Maven central is proxied through nexus
-    maven {
-        url = uri("http://nexus3.riscure.com:8081/repository/maven-central/")
-        isAllowInsecureProtocol = true
-    }
-
-    // "llvm-platform" is located in "3rdparty-snapshots" repo in nexus
-    maven  {
-        url = uri("http://nexus3.riscure.com:8081/repository/3rdparty-snapshots/")
-        isAllowInsecureProtocol = true
-    }
-
-    mavenLocal()
-}
-
 dependencies {
     implementation("org.bytedeco:llvm-platform:11.0.0-1.5.5-SNAPSHOT")
 
@@ -55,7 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation("info.picocli:picocli:4.6.3")
 
-    implementation("com.riscure:riscure-dobby:0.1.0-SNAPSHOT")
+    implementation("com.riscure:riscure-dobby:0.1.0")
     implementation(project(":bumper-core"))
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
@@ -85,7 +51,14 @@ tasks.compileTestKotlin {
     }
 }
 
-// Publishing
+fun env(key: String): String? = System.getenv(key)
+
+val nexusUsername = env("NEXUS_USERNAME")
+val nexusPassword = env("NEXUS_PASSWORD")
+
+val releases  = uri("http://nexus3.riscure.com:8081/repository/riscure")
+val snapshots = uri("http://nexus3.riscure.com:8081/repository/riscure-snapshots")
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
@@ -104,9 +77,12 @@ publishing {
 
     repositories {
         maven {
-            url = uri(bambooLocal)
-            name = "localBamboo"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshots else releases)
             isAllowInsecureProtocol = true
+            credentials {
+                username = nexusUsername
+                password = nexusPassword
+            }
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,53 @@ pluginManagement {
         // If it fails, we default on gradle plugin portal (not visible from bamboo, requires internet)
         gradlePluginPortal()
     }
+
+    dependencyResolutionManagement {
+        repositories {
+            maven {
+                url = uri("http://nexus3.riscure.com:8081/repository/riscure")
+                isAllowInsecureProtocol = true
+            }
+
+            maven {
+                url = uri("http://nexus3.riscure.com:8081/repository/riscure-snapshots")
+                isAllowInsecureProtocol = true
+            }
+
+            maven {
+                url = uri("http://nexus3.riscure.com:8081/repository/maven-central/")
+                isAllowInsecureProtocol = true
+            }
+
+            maven {
+                url = uri("http://nexus3.riscure.com:8081/repository/3rdparty")
+                isAllowInsecureProtocol = true
+            }
+
+            maven {
+                url = uri("http://nexus3.riscure.com:8081/repository/3rdparty-snapshots")
+                isAllowInsecureProtocol = true
+            }
+        }
+
+        versionCatalogs {
+            create("kotlinx") {
+                library("json"                   , "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+                library("coroutines-core"        , "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+                library("serialization-core"     , "org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.4.1")
+                library("serialization-protobuf" , "org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.4.1")
+            }
+
+            create("libs") {
+                library("arrow.core"     , "io.arrow-kt:arrow-core:1.1.2")
+                library("apache.commons" , "org.apache.commons:commons-lang3:3.8.1")
+                library("slf4j"          , "org.slf4j:slf4j-api:1.7.25")
+                library("process"        , "com.github.pgreze:kotlin-process:1.4")
+                library("junit"          , "org.junit.jupiter:junit-jupiter:5.8.2")
+                library("dobby"          , "com.riscure:riscure-dobby:0.1.0")
+            }
+        }
+    }
 }
 
 rootProject.name = "bumper"
