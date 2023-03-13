35674f401bed7ee76669fd079f72a336fd525e99
Arjen Rouvoet <rouvoet@riscure.com>
2023-03-13 13:02:55 +0100

    Add utility for Clang frontend that sets -working_directory

--- implementations/libclang/src/main/kotlin/com/riscure/bumper/libclang/frontend.kt
+++ implementations/libclang/src/main/kotlin/com/riscure/bumper/libclang/frontend.kt
@@ -1,15 +1,37 @@
 package com.riscure.bumper.libclang
 
+import arrow.core.Either
+import arrow.core.flatMap
 import com.riscure.bumper.Frontend
 import com.riscure.bumper.Storage
 import com.riscure.bumper.preprocessor.impl.ClangPreprocessor
+import com.riscure.dobby.clang.Arg
+import com.riscure.dobby.clang.CompilationDb
+import com.riscure.dobby.clang.Spec
+import org.bytedeco.llvm.clang.CXCursor
+import java.io.File
 import java.nio.file.Path
 
 /**
  * Create a pipeline based on clang tooling.
  */
-fun frontend(clang: Path, cppStorage: Storage) = Frontend(
-    ClangPreprocessor(clang),
-    ClangParser(),
-    cppStorage
-)
+fun frontend(clang: Path, cppStorage: Storage) = ClangFrontend(clang, cppStorage)
+
+class ClangFrontend(clang: Path, cppStorage: Storage):
+    Frontend<CXCursor, CXCursor, ClangUnitState>(ClangPreprocessor(clang), ClangParser(), cppStorage) {
+    fun process(main: File, cdb: CompilationDb): Either<Throwable, ClangUnitState> =
+        cdb
+            .get(main.toPath())
+            .toEither { RuntimeException("No compile command for file '$main'") }
+            .flatMap { entry ->
+                process(
+                    main,
+                    entry.command
+                        .replace(
+                            Spec.clang11,
+                            Arg(Spec.clang11["working_directory"], entry.workingDirectory.toString())
+                        )
+                        .optArgs
+                )
+            }
+}
