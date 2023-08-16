85aaefc31baf46ffa62889856ba0bc13b258fe55
Arjen Rouvoet <rouvoet@riscure.com>
2023-08-16 14:50:08 +0200

    Upgrade to Kotline 1.8.22

--- dobby/build.gradle.kts
+++ dobby/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.20" apply false
-    kotlin("plugin.serialization") version "1.8.20" apply false
+    kotlin("jvm") version "1.8.22" apply false
+    kotlin("plugin.serialization") version "1.8.22" apply false
     id("org.jetbrains.dokka") version "1.8.20"
 
     // for packaging build scripts writting in the kotlin dsl
